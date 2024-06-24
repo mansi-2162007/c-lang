89db64c3c14865151986afd928968318fdf57588
mansi-2162007 <nilamporvad@gmail.com>
2024-06-24 11:56:18 +0530

    Add files via upload

---
+++ identity1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main ()
+{
+	int x;
+	int y;
+		
+		
+		printf("enter x");
+		scanf("%d,x+y,x+y");
+		printf("enter y");
+		scanf("%d,x+y,x+y");
+}
---
+++ identity2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main ()
+
+{
+	int x;
+	int y;
+	
+	printf("enter y");
+	scanf("%d,2*x*y,x*y");
+	printf("enter x");
+	scanf("%d,2*x*y,x*y");
+
+	
+	
+}
